cbe37b47249389662654c6150e33b78fda52d3bb
Jerome Swannack <jeromeswannack@gmail.com>
2022-03-09 07:21:52 +1300

    added sliders

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,49 @@ let lastMs = 0.0
 let debugNextPrint = 1000
 let debugNextPrintStep = 100000
 
+
+let sliderSpacing = 35
+let sliderWidth = 200
+
+let sliderSpec = [
+    {
+        title: "Neighbourhood Radius",
+        settingKey: "distMax",
+        min: 0,
+        max: 600,
+        default: 200.0
+    },
+    {
+        title: "Maximum Velocity",
+        settingKey: "velocityMax",
+        min: 0,
+        max: 1000,
+        default: 300.0
+    },
+    {
+        title: "Separation",
+        settingKey: "separationFactor",
+        min: 0,
+        max: 50,
+        default: 10.0
+    },
+    {
+        title: "Cohesion",
+        settingKey: "cohesionFactor",
+        min: 0,
+        max: 10,
+        default: 1.0
+    },
+    {
+        title: "Alignment",
+        settingKey: "alignmentFactor",
+        min: 0,
+        max: 10,
+        default: 1.0
+    },
+]
+let sliders = {}
+
 WebAssembly.instantiateStreaming(fetch("boids.wasm"), go.importObject).then((result) => {
     go.run(result.instance);
 }).then(v => {
@@ -17,7 +60,27 @@ WebAssembly.instantiateStreaming(fetch("boids.wasm"), go.importObject).then((res
 
 function setup() {
     createCanvas(w, h)
+
+    for (let i = 0; i < sliderSpec.length; i++) {
+        let spec = sliderSpec[i]
+        let slider = createSlider(spec.min, spec.max, spec.default, 1)
+        slider.position(20, sliderSpacing*(i+1))
+        slider.style('width', `${sliderWidth}px`);
+        sliders[spec.settingKey] = slider
+    }
+
     lastMs = millis()
+}
+
+function createSlider(title, index){
+
+    let spacing = 20
+
+    let slider = createSlider(0, 800, 50, 1)
+    slider.position(20, sliderSpacing*(index+1))
+    slider.style('width', '200px');
+    text(title, slider.x * 2 + slider.width, sliderSpacing*(index+1));
+    return slider
 }
 
 function draw() {
@@ -30,11 +93,14 @@ function draw() {
     timeStep = (ms - lastMs) / 1000
     lastMs = ms
 
+    let settings = {}
+    for(var key in sliders) {
+        settings[key] = sliders[key].value()
+    }
+
     boids = updateBoids({
         timeStep,
-        settings: {
-            "distMax": 300.0
-        }
+        settings,
     })
     background(255)
     boids.boids.map(v => drawBoid(...v))
@@ -45,12 +111,24 @@ function draw() {
         boids.settings,
     ))
     
-
-
     if (ms > debugNextPrint) {
         debugNextPrint += debugNextPrintStep
         console.log(boids)
     }
+
+    drawSliderText()
+}
+
+function drawSliderText() {
+    push()
+    stroke("black")
+    strokeWeight(1)
+    for (let i = 0; i < sliderSpec.length; i++) {
+        let spec = sliderSpec[i]
+        let slider = sliders[spec.settingKey]
+        text(spec.title, slider.x + slider.width + 10, sliderSpacing*(i+1) + 15);
+    }
+    pop()
 }
 
 function drawDebugBoid(boid, neighbours, settings) {
@@ -61,7 +139,7 @@ function drawDebugBoid(boid, neighbours, settings) {
     circle(boid[0], boid[1], 20)
     circle(boid[0], boid[1], settings.distMax*2)
 
-    strokeWeight(2)
+    strokeWeight(1)
     neighbours.map(b => circle(b[0], b[1], 20))
 
     pop()
